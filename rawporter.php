a28f0cc41e88ea2ef1ce816046fe70e5696965d6
rawporter <rawporter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-30 11:57:20 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rawporter/tags/1.0@550835 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ rawporter.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @package rawporter
+ * @version 1.0
+ */
+/*
+Plugin Name: Rawporter Request Media
+Plugin URI: http://wordpress.org/extend/plugins/rawporter/
+Description: For rawporter.com page popup on blog.
+Author: GriffeY
+Version: 1.0
+Author URI: http://rawporter.com/
+*/
+function rawporter_box() {
+	//loading required JS, CSS and initialize it
+?>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>	
+	<script type="text/javascript" src="<?php echo plugins_url('/fancybox/jquery.fancybox.pack.js',__FILE__ ); ?>"></script>	 
+	<link rel="stylesheet" href="<?php echo plugins_url('/fancybox/jquery.fancybox.css',__FILE__ ); ?>" type="text/css" media="screen" /> 	      
+	<div id="butDiv">
+		<a id="lnkButDiv" href="<?php echo "http://rawporter.com/wpassign";?>" class="fancybox fancybox.iframe">
+			<input type="button"  class="button-primary" value="Request Media" />
+		</a>	
+	</div>
+	<script type="text/javascript">
+        $(document).ready(function() {
+            $('.fancybox').fancybox({
+                padding : 0,
+                openEffect  : 'none',				
+				openSpeed  : 'fast'
+            });
+        });
+    </script>
+<?php
+}
+function rawporter_exec() {	
+	add_meta_box( 'rawporter_boxid', __('Request Media', 'wpsc'), 'rawporter_box', $pagename, 'side', 'low' );
+}
+// This just echoes the chosen line, we'll position it later
+function rawporter() {
+	$chosen = rawporter_exec();
+	echo "<p id='rawporter'>$chosen</p>";
+}
+// Now we set that function up to execute when the admin_notices action is called
+add_action( 'admin_notices', 'rawporter' );
+
+// We need some CSS to position the div
+function rawporter_css() {
+	// This makes sure that the positioning is also good for right-to-left languages
+	$x = is_rtl() ? 'left' : 'right';
+	echo "
+	<style type='text/css'>
+	#rawporter {
+		float: $x;
+		padding-$x: 15px;
+		padding-top: 5px;		
+		margin: 0;
+		font-size: 11px;
+	}	
+	#lnkButDiv {
+		text-decoration:none;
+	}
+	</style>
+	";
+}
+add_action( 'admin_head', 'rawporter_css' );
+?>
